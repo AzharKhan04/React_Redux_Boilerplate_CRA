b3a159f41f3b963bba0d1e54960552286864ac03
azharkhan04 <azharkhan.itech@gmail.com>
2021-10-18 17:55:31 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import React from 'react';
+import Welcome from './Welcome';
+
+function App() {
+
+    return ( 
+        <Welcome></Welcome> 
+    );
+}
+
+export default App;
